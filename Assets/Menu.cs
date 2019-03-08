c5a1aaa30a41fb85c766a91e7616755e7b8f5a7d
ashfaqueck <ashfaqueonmail@gmail.com>
2019-03-08 23:36:56 +0530

    base

---
+++ Assets/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public DefaultTrackableEventHandler tracker;
+    public Text text;
+
+
+    public void toggleModel()
+    {
+        if (tracker.wolf.activeSelf)
+        {
+            tracker.wolf.SetActive(false);
+            tracker.phoenix.SetActive(true);
+            text.text = "Phoenix Activated";
+        }
+
+        else
+        {
+            tracker.phoenix.SetActive(false);
+            tracker.wolf.SetActive(true);
+            text.text = "Wolf Activated";
+        }
+    }
+}
